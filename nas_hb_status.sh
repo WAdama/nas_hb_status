05850aad2516668606cc592e66ab5143488af5a8
Ingo <foren@asche-rz.de>
2020-07-14 15:44:14 +0200

    Test (#2)
    * Added new values
    
    Added check for backup integrity
    Test added whether backup has already been run
    
    * Update nas_hb_status.sh
    
    * Add files via upload
    
    * Update README.md
    
    * Delete nas_hb_status_sensor.png
    
    * Add files via upload

--- nas_hb_status.sh
+++ nas_hb_status.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 1.1.0
+# Version 1.2.0
 
 CONF=$1
 source $CONF
@@ -12,10 +12,17 @@ for TASK in "${TASKS[@]}"
 do
 
 CONTENT=`cat $LOG | grep "task" | grep "\[$TASK\]" | tail -1`
+INTEGRITY=`cat $LOG | grep "Backup integrity check" | grep "\[$TASK\]" | tail -1`
 TASKID=`cat $SYSLOG | grep "task" | grep "\[$TASK\]" | tail -1 | sed -n "s/^.*img_backup: (\s*\([0-9]*\).*$/\1/p"`
-RUNTIME=`cat $SYSLOG | grep "task" | grep "\[$TASK\]" | tail -1 | sed -n "s/^.*Time spent: \[\s*\([0-9]*\).*$/\1/p"`
-BKPSIZE=`cat $SYSLOG | grep "img_backup" | grep "$TASKID" | grep "Storage Statistics" | tail -1 | sed -n "s/^.*: TargetSize(KB):\[\s*\([0-9]*\).*$/\1/p"`
-LASTBKPSIZE=`cat $SYSLOG | grep "img_backup" | grep "$TASKID" | grep "Storage Statistics" | tail -1 | sed -n "s/^.*LastBackupTargetSize(KB):\[\s*\([0-9]*\).*$/\1/p"`
+if [ -z "${TASKID}" ]; then
+	RUNTIME="0"
+	BKPSIZE="0"
+	LASTBKPSIZE="0"
+	else
+	RUNTIME=`cat $SYSLOG | grep "task" | grep "\[$TASK\]" | tail -1 | sed -n "s/^.*Time spent: \[\s*\([0-9]*\).*$/\1/p"`
+	BKPSIZE=`cat $SYSLOG | grep "img_backup" | grep "$TASKID" | grep "Storage Statistics" | tail -1 | sed -n "s/^.*: TargetSize(KB):\[\s*\([0-9]*\).*$/\1/p"`
+	LASTBKPSIZE=`cat $SYSLOG | grep "img_backup" | grep "$TASKID" | grep "Storage Statistics" | tail -1 | sed -n "s/^.*LastBackupTargetSize(KB):\[\s*\([0-9]*\).*$/\1/p"`
+fi
 TIME=`cat $LOG | grep "task" | grep "\[$TASK\]" | grep -o "[0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}" | tail -1`
 
 TIMEEND=`date -d"$TIME" +%s`
@@ -36,12 +43,22 @@ if [[ $CONTENT == *"finished successfully"* ]]; then
 	STATUS="7"
 fi
 
+if [[ $INTEGRITY == *"No error was found"* ]]; then
+	INTSTATUS="1"
+	elif [[ $INTEGRITY == *"has started"* ]]; then
+	INTSTATUS="2"
+	elif [[ $INTEGRITY == *"target is found broken"* ]]; then
+	INTSTATUS="3"
+	else
+	INTSTATUS="0"
+fi
+
 ACTTIME=`date +%s`
 LASTRUN=$(($ACTTIME - $TIMEEND))
 
 BKPCHANGE=$(($BKPSIZE - $LASTBKPSIZE))
 
-echo "<result><channel>$TASK: Time passed</channel><value>$LASTRUN</value><unit>TimeSeconds</unit><LimitMode>1</LimitMode><LimitMaxWarning>129600</LimitMaxWarning><LimitMaxError>216000</LimitMaxError></result><result><channel>$TASK: Status</channel><value>$STATUS</value><ValueLookup>prtg.standardlookups.nas.hbstatus</ValueLookup><ShowChart>0</ShowChart></result><result><channel>$TASK: Runtime</channel><value>$RUNTIME</value><unit>TimeSeconds</unit></result><result><channel>$TASK: Size</channel><value>$(($BKPSIZE * 1024))</value><unit>BytesDisk</unit></result><result><channel>$TASK: Change</channel><value>$(($BKPCHANGE * 1024))</value><unit>BytesDisk</unit></result>"
+echo "<result><channel>$TASK: Time passed</channel><value>$LASTRUN</value><unit>TimeSeconds</unit><LimitMode>1</LimitMode><LimitMaxWarning>129600</LimitMaxWarning><LimitMaxError>216000</LimitMaxError></result><result><channel>$TASK: Status</channel><value>$STATUS</value><ValueLookup>prtg.standardlookups.nas.hbstatus</ValueLookup><ShowChart>0</ShowChart></result><result><channel>$TASK: Runtime</channel><value>$RUNTIME</value><unit>TimeSeconds</unit></result><result><channel>$TASK: Size</channel><value>$(($BKPSIZE * 1024))</value><unit>BytesDisk</unit></result><result><channel>$TASK: Change</channel><value>$(($BKPCHANGE * 1024))</value><unit>BytesDisk</unit></result><result><channel>$TASK: Integrity Check</channel><value>$INTSTATUS</value><ValueLookup>prtg.standardlookups.nas.hbintstatus</ValueLookup><ShowChart>0</ShowChart></result>"
 done
 echo "</prtg>"
 exit
