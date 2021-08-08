fa17c742f8bfda449805d287fc87c5b6ab0ff19d
Ingo <foren@asche-rz.de>
2021-08-08 18:44:52 +0200

    Some optimization

--- nas_hb_status.sh
+++ nas_hb_status.sh
@@ -1,73 +1,62 @@
 #!/bin/bash
-# Version 1.2.6
+# Version 1.2.7
 
-CONF=$1
-source $CONF
+source $1
 
 echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg>"
 for TASK in "${TASKS[@]}"
 do
-CONTENT=`cat $LOG | grep "Backup task" | grep "\[$TASK\]" | tail -1`
+CONTENT=$(awk "/Backup task/ && /\[$TASK\]/" $LOG | tail -1)
 if [ -z "${CONTENT}" ]; then
-	CONTENT=`cat $LOGROTATED | grep "Backup task" | grep "\[$TASK\]" | tail -1`
+	CONTENT=$(awk "/Backup task/ && /\[$TASK\]/" $LOGROTATED | tail -1)
 fi
-INTEGRITY=`cat $LOG | grep "integrity check" | grep "\[$TASK\]" | tail -1`
+INTEGRITY=$(awk "/integrity check/ && /\[$TASK\]/" $LOG | tail -1)
 if [ -z "${INTEGRITY}" ]; then
-	INTEGRITY=`cat $LOGROTATED | grep "integrity check" | grep "\[$TASK\]" | tail -1`
+	INTEGRITY=$(awk "/integrity check/ && /\[$TASK\]/" $LOGROTATED | tail -1)
 fi
-TASKID=`cat $SYSLOG | grep "Backup task" | grep "\[$TASK\]" | tail -1 | sed -n "s/^.*: (\s*\([0-9]*\).*$/\1/p"`
+TASKID=$(awk "/Backup task/ && /\[$TASK\]/" $SYSLOG | tail -1 | sed -n "s/^.*: (\s*\([0-9]*\).*$/\1/p")
 if [ -z "${TASKID}" ]; then
 	RUNTIME="0"
 	BKPSIZE="0"
 	LASTBKPSIZE="0"
 	else
-	RUNTIME=`cat $SYSLOG | grep "Backup task" | grep "\[$TASK\]" | tail -1 | sed -n "s/^.*Time spent: \[\s*\([0-9]*\).*$/\1/p"`
-	BKPSIZE=`cat $SYSLOG | grep "img_backup" | grep "$TASKID" | grep "Storage Statistics" | tail -1 | sed -n "s/^.*: TargetSize(KB):\[\s*\([0-9]*\).*$/\1/p"`
-	LASTBKPSIZE=`cat $SYSLOG | grep "img_backup" | grep "$TASKID" | grep "Storage Statistics" | tail -1 | sed -n "s/^.*LastBackupTargetSize(KB):\[\s*\([0-9]*\).*$/\1/p"`
+	RUNTIME=$(awk "/Backup task/ && /\[$TASK\]/" $SYSLOG | tail -1 | sed -n "s/^.*Time spent: \[\s*\([0-9]*\).*$/\1/p")
+	BKPSIZE=$(awk "/img_backup/ && /$TASKID/ && /Storage Statistics/" $SYSLOG | tail -1 | sed -n "s/^.*: TargetSize(KB):\[\s*\([0-9]*\).*$/\1/p")
+	LASTBKPSIZE=$(awk "/img_backup/ && /$TASKID/ && /Storage Statistics/" $SYSLOG | tail -1 | sed -n "s/^.*LastBackupTargetSize(KB):\[\s*\([0-9]*\).*$/\1/p")
 fi
-TIME=`cat $LOG | grep "Backup task" | grep "\[$TASK\]" | grep -o "[0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}" | tail -1`
+TIME=$(awk "/Backup task/ && /\[$TASK\]/" $LOG | tail -1 | grep -o "[0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}")
 if [ -z "${TIME}" ]; then
-	TIME=`cat $LOGROTATED | grep "Backup task" | grep "\[$TASK\]" | grep -o "[0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}" | tail -1`
+	TIME=$(awk "/Backup task/ && /\[$TASK\]/" $LOGROTATED | tail -1 | grep -o "[0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}")
 fi
-INTTIME=`cat $LOG | grep "Backup integrity check" | grep "\[$TASK\]" | grep -o "[0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}" | tail -1`
+INTTIME=$(awk "/Backup integrity check/ && /\[$TASK\]/" $LOG | tail -1 | grep -o "[0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}")
 if [ -z "${INTTIME}" ]; then
-	INTTIME=`cat $LOGROTATED | grep "Backup integrity check" | grep "\[$TASK\]" | grep -o "[0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}" | tail -1`
+	INTTIME=$(awk "/Backup integrity check/ && /\[$TASK\]/" $LOGROTATED | tail -1 | grep -o "[0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}")
+
 
 fi
-TIMEEND=`date -d "$TIME" +%s`
-INTTIMEEND=`date -d "$INTTIME" +%s`
+TIMEEND=$(date -d "$TIME" +%s)
+INTTIMEEND=$(date -d "$INTTIME" +%s)
 
-if [[ $CONTENT == *"finished successfully"* ]]; then
-	STATUS="1"
-	elif [[ $CONTENT == *"Failed"* ]]; then
-	STATUS="2"
-	elif [[ $CONTENT == *"created"* ]]; then
-	STATUS="3"
-	elif [[ $CONTENT == *"started"* ]]; then
-	STATUS="4"
-	elif [[ $CONTENT == *"cancelled"* ]]; then
-	STATUS="5"
-	elif [[ $CONTENT == *"suspension complete"* ]]; then
-	STATUS="6"
-	elif [[ $CONTENT == *"resume backup"* ]]; then
-	STATUS="7"
-	elif [[ $CONTENT == *"partially completed"* ]]; then
-	STATUS="8"
-fi
+case $CONTENT in
+	*"finished successfully"*) STATUS="1" 	;;
+	*"Failed"*) STATUS="2" ;;
+	*"created"*) STATUS="3" ;;
+	*"started"*) STATUS="4" ;;
+	*"cancelled"*) STATUS="5" ;;
+	*"suspension complete"*) STATUS="6" ;;
+	*"resume backup"*) STATUS="7" ;;
+	*"partially completed"*) STATUS="8" ;;
+esac
 
-if [[ $INTEGRITY == *"No error was found"* ]]; then
-	INTSTATUS="1"
-	elif [[ $INTEGRITY == *"has started"* ]]; then
-	INTSTATUS="2"
-	elif [[ $INTEGRITY == *"target is found broken"* ]]; then
-	INTSTATUS="3"
-	elif [[ $INTEGRITY == *"Failed to run backup integrity check"* ]]; then
-	INTSTATUS="4"
-	else
-	INTSTATUS="0"
-fi
+case $INTEGRITY in
+	*"No error was found"*) INTSTATUS="1" 	;;
+	*"has started"*) INTSTATUS="2" ;;
+	*"target is found broken"*) INTSTATUS="3" ;;
+	*"Failed to run backup integrity check"*) INTSTATUS="4" ;;
+	*) INTSTATUS="0" ;;
+esac
 
-ACTTIME=`date +%s`
+ACTTIME=$(date +%s)
 LASTRUN=$(($ACTTIME - $TIMEEND))
 INTLASTRUN=$(($ACTTIME - $INTTIMEEND))
 
